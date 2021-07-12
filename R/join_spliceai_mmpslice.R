cb07a7f28a8dc5073d78c213e90c34b2fa26e1f5
Lang <franlang@tronlogin.zdv.uni-mainz.de>
2021-07-12 18:22:55 +0200

    add function to combine mutations res

---
+++ R/join_spliceai_mmpslice.R
@@ -0,0 +1,42 @@
+
+#' Combines tibbles with junctions from SpliceAI and MMsplice
+#'
+#' @param spliceai_juncs A tibble the junctions identified by SpliceAI
+#' @param mmsplice_juncs A tibble the junctions identified by MMsplice in
+#'   standardized format
+#'
+#' @return A combined table with unique junctions. The columns DNA_tool
+#'   contains information which tools identified the given junction
+#'
+#' @examples
+#'
+#' @import dplyr
+#' @export
+combine_mut_junc <- function(spliceai_juncs, mmsplice_juncs){
+
+  spliceai_juncs <- spliceai_juncs %>%
+    mutate(junc_tx_id = paste0(junc_id, "_", tx_id))%>%
+    dplyr::rename(prob_spliceai=prob)%>%
+    dplyr::rename(class_spliceai=class)
+
+  mmsplice_juncs <- mmsplice_juncs %>%
+    mutate(junc_tx_id = paste0(junc_id, "_", transcript_id),
+           tx_id = transcript_id)%>%
+    dplyr::rename(delta_logit_psi_mmsplice = delta_logit_psi) %>%
+    dplyr::rename(pathogenicity_mmsplice = pathogenicity)%>%
+    dplyr::rename(class_mmsplice = effect)%>%
+    dplyr::rename(efficiency_mmsplice = efficiency)
+
+  juncs <- bind_rows(spliceai_juncs, mmsplice_juncs)
+  juncs <- juncs %>%
+    distinct(junc_tx_id, .keep_all = T) %>%
+    mutate(identified_by_spliceai = ifelse(junc_tx_id %in% spliceai_juncs$junc_tx_id, TRUE, FALSE),
+           identified_by_mmsplice = ifelse(junc_tx_id %in% mmsplice_juncs$junc_tx_id, TRUE, FALSE )) %>%
+    select(junc_tx_id, mut_id, tx_id, junc_id, junc_tx_id,
+           identified_by_spliceai, identified_by_mmsplice,
+           prob_spliceai, class_spliceai, delta_logit_psi_mmsplice,
+           pathogenicity_mmsplice, efficiency_mmsplice,  class_mmsplice) %>%
+    filter(!is.na(junc_id))
+  return(juncs)
+
+}
