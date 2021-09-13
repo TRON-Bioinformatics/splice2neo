afa81da7affbf9343eb3b0fa2f8fe2ee1bd75c6a
Jonas Ibn-Salem <jonas.ibn-salem@tron-mainz.de>
2021-09-13 18:44:46 +0200

    fix junc_to_peptide() with tx_id as input. Relates to #13

--- tests/testthat/test-junc_to_peptide.R
+++ tests/testthat/test-junc_to_peptide.R
@@ -21,18 +21,18 @@ test_that("junc_to_peptide works on example data", {
 
 test_that("junc_to_peptide works on example data with input tx ids", {
 
-  # toy_junc_id
-  # toy_junc_id_enst
-  # toy_cds
-
   requireNamespace("BSgenome.Hsapiens.UCSC.hg19", quietly = TRUE)
   bsg <- BSgenome.Hsapiens.UCSC.hg19::BSgenome.Hsapiens.UCSC.hg19
 
-  pep_df <- junc_to_peptide(toy_junc_id, toy_cds, size = 30, bsg = bsg)
+  junc_id <- toy_junc_id[c(1, 6, 10)]
+  tx_id <- toy_junc_id_enst[c(1, 6, 10)]
+  cds <- toy_cds
+
+  pep_df <- junc_to_peptide(junc_id, cds, tx_id = tx_id, size = 30, bsg = bsg)
 
   expect_true(nrow(pep_df) > 0)
-  expect_true(nrow(pep_df) >= length(toy_junc_id))
-  expect_equal(unique(pep_df$junc_id), unique(toy_junc_id))
+  expect_true(nrow(pep_df) >= length(junc_id))
+  expect_equal(unique(pep_df$junc_id), unique(junc_id))
 
   # check that peptides have expcted size
   expect_true(all(stringr::str_length(pep_df$peptide_context) <= 30, na.rm = TRUE))
