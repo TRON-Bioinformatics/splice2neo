960455fd08470a36bf0fec9c756b67a49892dcee
Jonas Ibn-Salem <jonas.ibn-salem@tron-mainz.de>
2021-11-24 20:41:15 +0100

    fix parse_gtf() by suppressing warning

--- R/parse_gtf.R
+++ R/parse_gtf.R
@@ -5,16 +5,26 @@
 #' @param file path to a GFF or GTF file (See \code{\link[makeTxDbFromGFF]{GenomicFeatures}}))
 #' @return A \code{\link[GenomicRanges]{GRangesList}} of exons grouped by transcripts
 #'
+#' @examples
+#' gff_file <- system.file("extdata","GFF3_files","a.gff3",package="GenomicFeatures")
+#'
+#' parse_gtf(gff_file)
+#'
 #' @export
 parse_gtf <- function(file){
 
-  txdb <- GenomicFeatures::makeTxDbFromGFF(file)
+  txdb <- suppressWarnings(GenomicFeatures::makeTxDbFromGFF(file))
   transcripts <- GenomicFeatures::exonsBy(txdb, by = c("tx"), use.names = TRUE)
 
   return(transcripts)
-
 }
 
-canonical_junctions <- function(grl){
+#' Build canonical junctions from transcripts
+#'
+#' @param tx a \code{\link[GenomicRanges]{GRangesList}} of reference transcripts
+#'
+#' @return a character vector of canonical splice junction ids
+#'
+canonical_junctions <- function(tx){
 
 }
