5190b0d964a47a641baa8058b48da9a98ac170ae
Jonas Ibn-Salem <jonas.ibn-salem@tron-mainz.de>
2021-12-08 09:49:34 +0100

    remove test.bed as it can be used from system.file("tests","test.bed", package = "rtracklayer")

--- tests/testthat/test-bed_to_junc.R
+++ tests/testthat/test-bed_to_junc.R
@@ -1,6 +1,6 @@
 test_that("bed_to_junc works on rtracklayer test file", {
 
-  test_bed <- system.file("extdata","test.bed", package = "splice2neo")
+  test_bed <- system.file("tests","test.bed", package = "rtracklayer")
   junc_ids <- bed_to_junc(test_bed)
 
   expect_true(length(junc_ids) == 5)
@@ -10,7 +10,7 @@ test_that("bed_to_junc works on rtracklayer test file", {
 
 test_that("bed_to_junc works on rtracklayer test file with intron", {
 
-  test_bed <- system.file("extdata","test.bed", package = "splice2neo")
+  test_bed <- system.file("tests","test.bed", package = "rtracklayer")
   junc_ids <- bed_to_junc(test_bed, type = "intron")
 
   expect_true(length(junc_ids) == 5)
