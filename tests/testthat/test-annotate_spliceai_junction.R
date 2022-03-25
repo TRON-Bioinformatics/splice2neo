ddce8f1bcd904531a260029598e88e64d40b2f6f
Jonas Ibn-Salem <jonas.ibn-salem@tron-mainz.de>
2022-03-25 19:07:21 +0100

    update annotate_spliceai_junction() to handle donor gain and acceptor gain in introns

--- tests/testthat/test-annotate_spliceai_junction.R
+++ tests/testthat/test-annotate_spliceai_junction.R
@@ -9,3 +9,130 @@ test_that("annotate_spliceai_junction works on toy example", {
   expect_true(nrow(annot_df) >= nrow(df))
 
 })
+
+
+test_that("annotate_spliceai_junction works for multiple effects from same mutation", {
+
+  testthat::skip("Not implemented")
+
+  # spliceai_file <- "/projects/SUMMIT/WP1.1/splice/temp/input.vcf.spliceAI.vcf"
+  gtf_url <- "ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_human/release_34/GRCh37_mapping/gencode.v34lift37.annotation.gtf.gz"
+  # parse GTF file as txdb object
+  txdb <- GenomicFeatures::makeTxDbFromGFF(gtf_url)
+  transcripts <- GenomicFeatures::exonsBy(txdb, by = c("tx"), use.names = TRUE)
+  transcripts_gr <- GenomicFeatures::transcripts(txdb)
+  #
+  # df_raw <- parse_spliceai(spliceai_file) %>%
+  #   dplyr::filter(POS == 62934431, ALT == "T") %>%
+  #   mutate(CHROM = str_c("chr", CHROM))
+  # df <- format_spliceai(df_raw)
+  #
+  df <- dplyr::tibble(
+    CHROM = c("chr2", "chr2"),
+    POS = c("62934431", "62934431"),
+    ID = c(NA_character_, NA_character_),
+    REF = c("G", "G"),
+    ALT = c("T", "T"),
+    QUAL = c(NA_character_, NA_character_),
+    FILTER = c(NA_character_, NA_character_),
+    Key = c(3L, 3L),
+    ALLELE = c("T", "T"),
+    change = structure(3:4, .Label = c("AG","AL", "DG", "DL"), class = "factor"),
+    prob = c(0.32, 0.95),
+    pos_rel = c(16L, -1L)
+  )
+
+  annot_df <- annotate_spliceai_junction(df, transcripts, transcripts_gr)
+
+  # expect_true(nrow(annot_df) >= nrow(df))
+
+})
+
+
+test_that("annotate_spliceai_junction works for donor gain and aceptor gain in introns", {
+
+  #=============================================================================
+  #          1         2         3         4
+  # 1234567890123456789012345678901234567890
+  # -#######------#########---#########-----  tx1 (positive strand)
+  #       |                                   DG exon
+  #       [=======]                           DG exon junction
+  #                 |                         AG exon
+  #        [========]                         AG exon junction
+  #          |                                DG intron
+  #          [====]                           DG intron junction
+  #            |                              AG intron
+  #        [===]                              AG intron junction
+  #                       |                   DL
+  #                       []                  DL intron-retention junction
+  #        [==================]               DL exon-skipping junction
+  #               |                           AL
+  #              []                           AL intron-retention junction
+  #        [==================]               AL exon-skipping junction
+  #                   |                       DL exon
+  #                   []                      DL exon IR (canonical) junction
+  #        [==================]               DL exon ES (canonical) junction
+  # 1234567890123456789012345678901234567890
+  #          1         2         3         4
+  # -#######------#########---#########-----  tx2 (negative strand)
+  #       |                                   DG exon
+  #                                           DG exon junction
+  #                 |                         AG exon
+  #                 [=========]               AG exon junction
+  #          |                                DG intron
+  #        [=]                                DG intron junction
+  #            |                              AG intron
+  #            [==]                           AG intron junction
+  #                       |                   DL
+  #                      []                   DL intron-retention junction
+  #        [==================]               DL exon-skipping junction
+  #               |                           AL
+  #               []                          AL intron-retention junction
+  #        [==================]               AL exon-skipping junction
+  #                   |                       DL exon
+  #                  []                       DL exon IR (canonical) junction
+  #        [==================]               DL exon ES (canonical) junction
+  #=============================================================================
+
+  toy_df <- dplyr::tribble(
+    ~POS, ~change, ~name,
+    7,    "DG",  "DG exon",
+    17,   "AG",  "AG exon",
+    10,   "DG",  "DG intron",
+    12,   "AG",  "AG intron",
+    23,   "DL",  "DL",
+    15,   "AL",  "AL",
+    19,   "DL",  "DL exon"
+    ) %>%
+    mutate(CHROM = "1", pos_rel = 0, REF = "G", ALT = "T")
+
+  toy_tx <- GenomicRanges::GRangesList(list(
+    tx1 = GenomicRanges::GRanges(
+      c("1", "1", "1"),
+      IRanges::IRanges(
+        c(2, 15, 27),
+        c(8, 23, 35),
+      ),
+      strand = c("+", "+", "+")
+    ),
+    tx2 = GenomicRanges::GRanges(
+      c("1", "1", "1"),
+      IRanges::IRanges(
+        c(2, 15, 27),
+        c(8, 23, 35),
+      ),
+      strand = c("-", "-", "-")
+    )
+  ))
+
+  expected_pos = c("1:7-15:+", "1:8-17:+", "1:10-15:+", "1:8-12:+", "1:23-24:+",
+                  "1:8-27:+", "1:14-15:+", "1:8-27:+", "1:19-20:+", "1:8-27:+")
+  expected_neg = c("1:17-27:-", "1:8-10:-", "1:12-15:-", "1:22-23:-", "1:8-27:-",
+                   "1:15-16:-", "1:8-27:-", "1:18-19:-", "1:8-27:-")
+  expected_jx = c(expected_pos, expected_neg)
+
+  annot_df <- annotate_spliceai_junction(toy_df, toy_tx, range(toy_tx))
+
+  expect_equal(sort(annot_df$junc_id), sort(expected_jx))
+
+})
