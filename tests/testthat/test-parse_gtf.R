757277ee145df1b897392cf8f789ad667c972708
Jonas Ibn-Salem <jonas.ibn-salem@tron-mainz.de>
2024-03-24 16:19:14 +0100

    add format argument when using parse_gtf()

--- tests/testthat/test-parse_gtf.R
+++ tests/testthat/test-parse_gtf.R
@@ -2,7 +2,7 @@ test_that("parse_gtf works with GFF file", {
 
   gff_file <- system.file("extdata","GFF3_files","a.gff3",package="GenomicFeatures")
 
-  grl <- parse_gtf(gff_file)
+  grl <- parse_gtf(gff_file, format = "gff3")
 
   expect_s4_class(grl, "CompressedGRangesList")
 })
@@ -12,7 +12,7 @@ test_that("parse_gtf works with GTF file", {
   gtf_file <- system.file("extdata","GTF_files","Aedes_aegypti.partial.gtf",
                          package="GenomicFeatures")
 
-  grl <- parse_gtf(gtf_file)
+  grl <- parse_gtf(gtf_file, format = "gtf")
 
   expect_s4_class(grl, "CompressedGRangesList")
 })
