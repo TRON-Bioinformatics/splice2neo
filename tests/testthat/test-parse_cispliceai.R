ed54b6ef25e0674792014c0d85193dc208b41622
Jonas Ibn-Salem <jonas.ibn-salem@tron-mainz.de>
2024-05-25 15:27:31 +0200

    add test for empty CI splice input file

--- tests/testthat/test-parse_cispliceai.R
+++ tests/testthat/test-parse_cispliceai.R
@@ -6,3 +6,12 @@ test_that("parse_cispliceai_thresh works on CI-SpliceAI example file", {
   expect_true(nrow(df) >= 10)
 
 })
+
+test_that("parse_cispliceai_thresh works on empty CI-SpliceAI example file", {
+
+  cispliceai_file <- system.file("extdata", "cispliceai_thresh_output.empty.vcf", package = "splice2neo")
+  df <- parse_cispliceai_thresh(cispliceai_file)
+
+  expect_true(nrow(df) == 1)
+
+})
